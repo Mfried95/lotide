46d148d2e4dbb8c90c6aa91957f14f609bdc0f23
mfried95 <m.friedman1995@gmail.com>
2023-04-03 15:19:31 -0400

    Added head.js

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("The same");
+  } else {
+    console.log("Does not match");
+  }
+};
+
+
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1,2);
+
+
+function head() {
+  console.log(assertEqual[0])
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
